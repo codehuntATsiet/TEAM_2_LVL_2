7563761f401d0babfb43cb01ac762f887448fb3f
codehuntATsiet <127717140+codehuntATsiet@users.noreply.github.com>
2023-03-15 14:54:42 +0530

    Add files via upload

---
+++ codefill.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+void printClue(int );
+int findVolume(int ,int[] );
+
+int main()
+{
+    // write a code to find the sum of volume of n number of sphere by creating a function findVolume()
+    // here n is the size of array or number of sphere
+    int n,a[];
+    // write a code to get inputs for n, a[]
+
+
+
+    int sum=findVolume(n,a);
+    printClue(sum);
+    return 0;
+}
+
+int findVolume(int n, int a[])
+{
+    int sum=0;
+    // complete the function findVolume()
+
+    
+
+
+    return sum;
+}
+
+
+
+void printClue(int x)
+{   printf("%d",x);
+    
+
+}
